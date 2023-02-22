39f7f47dd082cb408ae51d86bccd6d7701ab6011
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-02-22 13:47:56 -0300

    fix: responsive sizes

--- src/components/Busqueda.js
+++ src/components/Busqueda.js
@@ -1,93 +1,103 @@
-import React, { useState } from 'react';
-import { useFormik } from 'formik';
-import { useContext } from 'react';
-import ApiContext from '../context/ApiContext';
-import axios from 'axios';
-import CajaSuperHeroe from './CajaSuperHeroe';
-import { Redirect } from 'react-router';
-import Cargando from './Cargando';
-import '../pages/buscador.css'
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { useContext } from "react";
+import ApiContext from "../context/ApiContext";
+import axios from "axios";
+import CajaSuperHeroe from "./CajaSuperHeroe";
+import { Redirect } from "react-router";
+import Cargando from "./Cargando";
+import "../pages/buscador.css";
 
-const validate = values => {
-    /*
+const validate = (values) => {
+  /*
     Validamos que el campo no esté vacío.
     */
-    const errors = {};  //Manejar errores en los input
+  const errors = {}; //Manejar errores en los input
 
-    if (!values.nombre) {
-        errors.nombre = 'Ingresá algún caracter';
-    }
-    return errors;
+  if (!values.nombre) {
+    errors.nombre = "Ingresá algún caracter";
+  }
+  return errors;
 };
 
 const Busqueda = (props) => {
-    const url = useContext(ApiContext)
-    const [resultadoBusqueda, setResultadoBusqueda] = useState(null)
-    const [cargando, setCargando] = useState(false)
+  const url = useContext(ApiContext);
+  const [resultadoBusqueda, setResultadoBusqueda] = useState(null);
+  const [cargando, setCargando] = useState(false);
 
-    function obtenerHeroes(url) {
-        axios.get(url)
-            .then(function (response) {
-                // Manejar éxito
-                const promesa = response
-                setResultadoBusqueda(promesa.data.results)
-                setCargando(false)
-            })
-            .catch(function (error) {
-                // Manejar error
-                console.log(error);
-            })
-    }
+  function obtenerHeroes(url) {
+    axios
+      .get(url)
+      .then(function (response) {
+        // Manejar éxito
+        const promesa = response;
+        setResultadoBusqueda(promesa.data.results);
+        setCargando(false);
+      })
+      .catch(function (error) {
+        // Manejar error
+        console.log(error);
+      });
+  }
 
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      obtenerHeroes(`${url}/search/${values.nombre}`);
+      setCargando(true);
+    },
+  });
 
-    const formik = useFormik({
-        initialValues: {
-            nombre: '',
-        },
-        validate,
-        onSubmit: values => {
-            obtenerHeroes(`${url}/search/${values.nombre}`)
-            setCargando(true)
-        },
-    });
-
-
-    if (!localStorage.TOKEN) {
-        return <Redirect to='/' />
-    } else {
-        return (
-            <div className='w-100'>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className='d-flex flex-row justify-content-center align-items-center'>
-                        <label htmlFor="nombre">Nombre:</label>
-                        <input
-                            className='w-50 m-2'
-                            id="nombre"
-                            name="nombre"
-                            type="text"
-                            onChange={formik.handleChange}
-                            value={formik.values.nombre}
-                        />
-                        <button type='submit' className=' btn btn-outline-primary'>Buscar</button>
-                    </div>
-                    {formik.errors.nombre ? <div className='text-danger fw-bold text-center'>{formik.errors.nombre}</div> : null}
-                </form>
-                <div className='row d-flex justify-content-evenly '>
-                    {cargando ?
-                        <Cargando />
-                        : resultadoBusqueda == null ?
-                            null
-                            : resultadoBusqueda.map(superHeroe => {
-                                return <CajaSuperHeroe nombre={superHeroe.name} imagen={superHeroe.image.url} bando={superHeroe.biography.alignment} key={superHeroe.id} id={superHeroe.id} usuario={props.usuario} />
-                            })
-
-
-                    }
-                </div>
-
+  if (!localStorage.TOKEN) {
+    return <Redirect to="/" />;
+  } else {
+    return (
+      <div className="w-100">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="d-flex flex-row justify-content-center align-items-center form-input">
+            <label htmlFor="nombre">Nombre:</label>
+            <input
+              className="w-50 m-2"
+              id="nombre"
+              name="nombre"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.nombre}
+            />
+            <button type="submit" className=" btn btn-outline-primary">
+              Buscar
+            </button>
+          </div>
+          {formik.errors.nombre ? (
+            <div className="text-danger fw-bold text-center">
+              {formik.errors.nombre}
             </div>
-        );
-    }
+          ) : null}
+        </form>
+        <div className="row d-flex justify-content-evenly ">
+          {cargando ? (
+            <Cargando />
+          ) : resultadoBusqueda == null ? null : (
+            resultadoBusqueda.map((superHeroe) => {
+              return (
+                <CajaSuperHeroe
+                  nombre={superHeroe.name}
+                  imagen={superHeroe.image.url}
+                  bando={superHeroe.biography.alignment}
+                  key={superHeroe.id}
+                  id={superHeroe.id}
+                  usuario={props.usuario}
+                />
+              );
+            })
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Busqueda;
--- src/components/TarjetaHeroe.js
+++ src/components/TarjetaHeroe.js
@@ -1,64 +1,110 @@
-import React, { useState } from 'react';
-import Popup from 'reactjs-popup';
+import React, { useState } from "react";
+import Popup from "reactjs-popup";
 
 const TarjetaHeroe = (props) => {
-    const [open, setOpen] = useState(false);
-    const closeModal = () => setOpen(false);
+  const [open, setOpen] = useState(false);
+  const closeModal = () => setOpen(false);
 
-    return (
-        <div className='d-flex flex-column align-items-center'>
-
-            <section className='d-flex flex-column border border-3 rounded border-dark p-2 mi-equipo__tarjeta-stats'>
-                <h1 className='text-center'>{props.nombre}</h1>
-                <div className='mi-equipo__tarjeta-imagen'>
-                    <img src={props.imagen} alt="Imagen de Super-heroe" className=' img-fluid w-50 mx-auto rounded' />
-                </div>
-                <div className='d-flex justify-content-around'>
-                    <div className='d-flex flex-column'>
-                        <p>Inteligencia:</p>
-                        <p>Fuerza:</p>
-                        <p>Velocidad:</p>
-                        <p>Durabilidad:</p>
-                        <p>Poder:</p>
-                        <p>Combate: </p>
-                    </div>
-                    <div className='fw-bold'>
-                        <p>{props.estadisticas.intelligence === 'null' ? 'n/a' : props.estadisticas.intelligence}</p>
-                        <p>{props.estadisticas.strength === 'null' ? 'n/a' : props.estadisticas.strength}</p>
-                        <p>{props.estadisticas.speed === 'null' ? 'n/a' : props.estadisticas.speed}</p>
-                        <p>{props.estadisticas.durability === 'null' ? 'n/a' : props.estadisticas.durability}</p>
-                        <p>{props.estadisticas.power === 'null' ? 'n/a' : props.estadisticas.power}</p>
-                        <p>{props.estadisticas.combat === 'null' ? 'n/a' : props.estadisticas.combat}</p>
-                    </div>
-                </div>
-
-            </section >
-            {/* <button className='btn btn-primary boton w-75 m-1' onClick={() => setVerDetalles(true)} >Mostrar Detalles</button> */}
-            <div>
-                <button type="button" className="button btn btn-primary m-1 boton" onClick={() => setOpen(o => !o)}>
-                    Detalles
-                </button>
-                <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                    <div className="modo">
-                        <div className="tarjeta-heroe__detalles bg-dark text-light rounded p-3" >
-                            <h1>{props.nombre}</h1>
-                            <p className="m-2">Nombre completo: {props.biografia['full-name']}</p>
-                            <p className="m-2">Peso: {props.apariencia.weight[1]}</p>
-                            <p className="m-2">Altura: {props.apariencia.height[1]}</p>
-                            <p className="m-2">Color de ojos: {props.apariencia['eye-color']}</p>
-                            <p className="m-2">Color de pelo: {props.apariencia['hair-color']}</p>
-                            <p className="m-2">Lugar de Trabajo: {props.lugarTrabajo}</p>
-                            Aliases: {props.biografia.aliases.map((alias) => {
-                                return <p key={alias} className="m-1">- {alias}<br /></p>
-                            })
-                            }
-                            <button onClick={closeModal} className='btn btn-dark boton d-block mx-auto' >Cerrar</button >
-                        </div >
-                    </div>
-                </Popup>
+  return (
+    <div className="d-flex flex-column align-items-center w-100">
+      <section className="d-flex flex-column border border-3 rounded border-dark p-2 mi-equipo__tarjeta-stats">
+        <h1 className="text-center">{props.nombre}</h1>
+        <div className="mi-equipo__tarjeta-imagen">
+          <img
+            src={props.imagen}
+            alt="Imagen de Super-heroe"
+            className=" img-fluid w-50 mx-auto rounded"
+          />
+        </div>
+        <div className="d-flex justify-content-around">
+          <div className="d-flex flex-column">
+            <p>Inteligencia:</p>
+            <p>Fuerza:</p>
+            <p>Velocidad:</p>
+            <p>Durabilidad:</p>
+            <p>Poder:</p>
+            <p>Combate: </p>
+          </div>
+          <div className="fw-bold">
+            <p>
+              {props.estadisticas.intelligence === "null"
+                ? "n/a"
+                : props.estadisticas.intelligence}
+            </p>
+            <p>
+              {props.estadisticas.strength === "null"
+                ? "n/a"
+                : props.estadisticas.strength}
+            </p>
+            <p>
+              {props.estadisticas.speed === "null"
+                ? "n/a"
+                : props.estadisticas.speed}
+            </p>
+            <p>
+              {props.estadisticas.durability === "null"
+                ? "n/a"
+                : props.estadisticas.durability}
+            </p>
+            <p>
+              {props.estadisticas.power === "null"
+                ? "n/a"
+                : props.estadisticas.power}
+            </p>
+            <p>
+              {props.estadisticas.combat === "null"
+                ? "n/a"
+                : props.estadisticas.combat}
+            </p>
+          </div>
+        </div>
+      </section>
+      {/* <button className='btn btn-primary boton w-75 m-1' onClick={() => setVerDetalles(true)} >Mostrar Detalles</button> */}
+      <div className="w-75">
+        <button
+          type="button"
+          className="button btn btn-primary m-1 boton w-100"
+          onClick={() => setOpen((o) => !o)}
+        >
+          Detalles
+        </button>
+        <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+          <div className="modo">
+            <div className="tarjeta-heroe__detalles bg-dark text-light rounded p-3">
+              <h1>{props.nombre}</h1>
+              <p className="m-2">
+                Nombre completo: {props.biografia["full-name"]}
+              </p>
+              <p className="m-2">Peso: {props.apariencia.weight[1]}</p>
+              <p className="m-2">Altura: {props.apariencia.height[1]}</p>
+              <p className="m-2">
+                Color de ojos: {props.apariencia["eye-color"]}
+              </p>
+              <p className="m-2">
+                Color de pelo: {props.apariencia["hair-color"]}
+              </p>
+              <p className="m-2">Lugar de Trabajo: {props.lugarTrabajo}</p>
+              Aliases:{" "}
+              {props.biografia.aliases.map((alias) => {
+                return (
+                  <p key={alias} className="m-1">
+                    - {alias}
+                    <br />
+                  </p>
+                );
+              })}
+              <button
+                onClick={closeModal}
+                className="btn btn-dark boton d-block mx-auto"
+              >
+                Cerrar
+              </button>
             </div>
-        </div >
-    )
+          </div>
+        </Popup>
+      </div>
+    </div>
+  );
 };
 
 export default TarjetaHeroe;
